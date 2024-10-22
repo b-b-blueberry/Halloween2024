2b893ce8d4b44de3fdaea554b4a49c3f8083b2ef
blueberry <dearbblueberry@gmail.com>
2024-10-22 13:39:34 +1100

    Progress commit:
    - Move maze images to Work folder.
    - Fix event repeat behaviour.
    - Add i18n file with content translations.
    - Tokenise reusable content strings.
    - Add character dialogue after events.
    - Add Quit events.
    - Update maze spritesheet with details and variants.
    - Update maps with tile variants and added detail.
    - Add new sprite animations as unique map features.
    - Hide timer if obscuring player.
    - Simplify some methods.

--- Halloween2024/ModEntry.cs
+++ Halloween2024/ModEntry.cs
@@ -10,7 +10,9 @@ using StardewValley.Pathfinding;
 
 /*
 debug ebi Custom_blueberry_H2024_
-patch reload bbhm
+patch reload blueberry.H2024.CP
+cs return string.Join(`, `, Game1.player.eventsSeen);
+cs return string.Join(`, `, Game1.player.mailReceived);
 */
 
 namespace Halloween2024
@@ -24,6 +26,14 @@ namespace Halloween2024
 			Red = 2,
 			Blue = 3,
 			Orange = 4
+		}
+
+		public enum MapEnding
+		{
+			None = 0,
+			Success = 1,
+			Fail = 2,
+			Quit = 3
 		}
 
 		public class ModState
@@ -48,6 +58,10 @@ namespace Halloween2024
 		public static Dictionary<string, string>? Strings { get; private set; }
 		public static bool EventUp
 			=> Game1.currentLocation?.currentEvent?.playerControlSequenceID?.StartsWith(ModEntry.Strings.GetValueSafe("ID")) ?? false;
+		public static bool EventMap
+			=> Game1.currentLocation is not null
+			&& Game1.currentLocation.IsTemporary
+			&& Game1.currentLocation.getMapProperty(ModEntry.Strings.GetValueSafe("ID")) is not null;
 		public static int TotalMs
 			=> (int)Game1.currentGameTime.TotalGameTime.TotalMilliseconds;
 		public static bool IsNightmare
@@ -55,6 +69,7 @@ namespace Halloween2024
 		public static bool IsUnderwater
 			=> Game1.currentLocation?.HasMapPropertyWithValue("indoorWater") ?? false;
 
+		public const string ContentPackUniqueId = "blueberry.H2024.CP";
 		public const string StringsAssetKey = "Mods/blueberry/H2024/Strings";
 
 		public static ModEntry Instance { get; private set; }
@@ -63,7 +78,7 @@ namespace Halloween2024
 		{
 			ModEntry.Instance = this;
 
-			if (!helper.ModRegistry.IsLoaded("blueberry.H2024.CP"))
+			if (!helper.ModRegistry.IsLoaded(ModEntry.ContentPackUniqueId))
 			{
 				this.Monitor.Log("Can't load without my CP component! Did you copy BOTH folders in?", LogLevel.Error);
 				return;
@@ -75,6 +90,7 @@ namespace Halloween2024
 			helper.Events.GameLoop.SaveLoaded += this.GameLoop_SaveLoaded;
 			helper.Events.Player.Warped += this.Player_Warped;
 			helper.Events.Display.Rendered += this.Display_Rendered;
+			helper.Events.GameLoop.Saving += this.GameLoop_Saving;
 		}
 
 		public static void SetUpEventLocation(GameLocation where)
@@ -83,9 +99,6 @@ namespace Halloween2024
 			ModEntry.Strings = ModEntry.LoadStrings();
 
 			Color strToRgb(string str) => str.Split(' ').ToList().ConvertAll(int.Parse).ToArray() is int[] i ? new(i[0], i[1], i[2]) : Color.Transparent;
-
-			// TODO: Fix
-			// [22:58:05 ERROR bbhm.cs] required index 1 (ModEntry.State.Value.MapGoal > x) has value '70 69', which can't be parsed as a number
 
 			string error;
 			string[] args = where.Map.Properties[ModEntry.Strings.GetValueSafe("ID")].ToString().Split('/');
@@ -157,6 +170,12 @@ namespace Halloween2024
 			ModEntry.State.Value.Texture = null;
 		}
 
+		public static void RepeatUnfinishedEvents(Farmer who)
+		{
+			string id = ModEntry.Strings.GetValueSafe("ID");
+			who.eventsSeen.RemoveWhere(s => s.StartsWith(id) && !who.mailReceived.Contains(s));
+		}
+
 		public static Dictionary<string, string> LoadStrings()
 		{
 			return Game1.content.Load<Dictionary<string, string>>(ModEntry.StringsAssetKey);
@@ -215,7 +234,7 @@ namespace Halloween2024
 			return npc;
 		}
 
-		public static void TryCreateMapSprite(Event e)
+		public static void TryCreateSprite(Event e)
 		{
 			GameLocation where = Game1.currentLocation;
 			Farmer who = Game1.player;
@@ -227,8 +246,94 @@ namespace Halloween2024
 
 				if (ModEntry.State.Value.MapNumber == (int)MapNumber.Purple)
 				{
+					bool isDot = Game1.random.NextDouble() < 0.75;
+					Rectangle r = who.GetBoundingBox();
+					r.Inflate(Game1.tileSize * 7, Game1.tileSize * 7);
+					Vector2 global = Utility.getRandomPositionInThisRectangle(r, Game1.random);
+					Vector2 motion = new(0, 0.05f);
+					float scale = (float)(0.75f + Game1.random.NextDouble() * 0.5f);
+					e.aboveMapSprites.Add(new TemporaryAnimatedSprite(
+						textureName: "LooseSprites/Cursors",
+						sourceRect: isDot ? new(666, 866, 1, 1) : new(377, 1438, 10, 10),
+						position: global,
+						flipped: false,
+						alphaFade: 0f,
+						color: new(250, 125, 175))
+					{
+						alpha = 0.0001f,
+						alphaFade = -0.005f * 1000f / Game1.viewport.Width * scale,
+						alphaFadeFade = -0.00001f * 1000f / Game1.viewport.Width * scale,
+						motion = motion * scale,
+						acceleration = new Vector2(0f, 0f),
+						interval = 999999,
+						layerDepth = global.Y / 10000f,
+						rotationChange = (float)(-0.5 + Game1.random.NextDouble()) * scale / 10,
+						scale = Game1.pixelZoom * scale * (isDot ? 3 : 1)
+					});
+
 					if (Game1.random.NextDouble() < spriteChance * 10)
 						Game1.playSound(new [] { "dogs", "distantTrain", "thunder_small" }[Game1.random.Next(3)]);
+				}
+				else if (ModEntry.State.Value.MapNumber == (int)MapNumber.Red)
+				{
+					if (Game1.random.NextDouble() < 0.25)
+						return;
+
+					Vector2 local = new Vector2(x: Game1.random.NextDouble() < 0.5 ? 0 : Game1.viewport.Width - 32, y: Game1.random.Next(Game1.viewport.Height));
+					float speed = 3f;
+					Rectangle r = who.GetBoundingBox();
+					r.Inflate(Game1.tileSize * 2, Game1.tileSize * 2);
+					Vector2 motion = Utility.getVelocityTowardPoint(zero + local, Utility.getRandomPositionInThisRectangle(r, Game1.random), speed);
+					float scale = (float)(0.75f + Game1.random.NextDouble() * 0.5f);
+					if (Game1.random.NextDouble() < 0.25)
+					{
+						e.aboveMapSprites.Add(new TemporaryAnimatedSprite(
+							textureName: "Characters/Monsters/Bat",
+							sourceRect: new Rectangle(0, 0, 16, 32),
+							position: local + zero,
+							flipped: false,
+							alphaFade: 0f,
+							color: new(250, 125, 175))
+						{
+							alphaFade = 0.001f * 1000f / Game1.viewport.Width,
+							alpha = 1f,
+							motion = motion * scale,
+							acceleration = new Vector2(0f, 0f),
+							interval = 60,
+							animationLength = 4,
+							totalNumberOfLoops = 999999,
+							layerDepth = local.Y / 10000f,
+							scale = 4f * scale
+						});
+						if (Game1.random.NextDouble() < spriteChance * 10)
+							Game1.playSound("batScreech");
+					}
+					else
+					{
+						e.aboveMapSprites.Add(new TemporaryAnimatedSprite(
+							textureName: "Characters/Monsters/Carbon Ghost",
+							sourceRect: new Rectangle(0, 0, 16, 24),
+							position: local + zero,
+							flipped: false,
+							alphaFade: 0f,
+							color: new(250, 25, 50))
+						{
+							alphaFade = 0.001f * 1000f / Game1.viewport.Width,
+							alpha = 1f * scale,
+							motion = motion * scale / 4f,
+							acceleration = new Vector2(0f, 0f),
+							interval = 500 * scale,
+							animationLength = 4,
+							totalNumberOfLoops = 999999,
+							layerDepth = local.Y / 10000f,
+							scale = 4f * scale,
+							yPeriodic = true,
+							yPeriodicLoopTime = 2000 * scale,
+							yPeriodicRange = 24 * scale
+						});
+						if (Game1.random.NextDouble() < spriteChance * 10)
+							Game1.playSound("ghost");
+					}
 				}
 				else if (ModEntry.State.Value.MapNumber == (int)MapNumber.Blue)
 				{
@@ -302,64 +407,7 @@ namespace Halloween2024
 						});
 					}
 				}
-				else if (ModEntry.State.Value.MapNumber == (int)MapNumber.Red)
-				{
-					Vector2 local = new Vector2(x: Game1.random.NextDouble() < 0.5 ? 0 : Game1.viewport.Width - 32, y: Game1.random.Next(Game1.viewport.Height));
-					float speed = 3f;
-					Rectangle r = who.GetBoundingBox();
-					r.Inflate(Game1.tileSize * 2, Game1.tileSize * 2);
-					Vector2 motion = Utility.getVelocityTowardPoint(zero + local, Utility.getRandomPositionInThisRectangle(r, Game1.random), speed);
-					float scale = (float)(0.75f + Game1.random.NextDouble() * 0.5f);
-					if (Game1.random.NextDouble() < 0.25)
-					{
-						e.aboveMapSprites.Add(new TemporaryAnimatedSprite(
-							textureName: "Characters/Monsters/Bat",
-							sourceRect: new Rectangle(0, 0, 16, 32),
-							position: local + zero,
-							flipped: false,
-							alphaFade: 0f,
-							color: Color.White)
-						{
-							alphaFade = 0.001f * 1000f / Game1.viewport.Width,
-							alpha = 1f,
-							motion = motion * scale,
-							acceleration = new Vector2(0f, 0f),
-							interval = 60,
-							animationLength = 4,
-							totalNumberOfLoops = 999999,
-							layerDepth = local.Y / 10000f,
-							scale = 4f * scale
-						});
-						if (Game1.random.NextDouble() < spriteChance * 10)
-							Game1.playSound("batScreech");
-					}
-					else
-					{
-						e.aboveMapSprites.Add(new TemporaryAnimatedSprite(
-							textureName: "Characters/Monsters/Carbon Ghost",
-							sourceRect: new Rectangle(0, 0, 16, 24),
-							position: local + zero,
-							flipped: false,
-							alphaFade: 0f,
-							color: new(250, 50, 50))
-						{
-							alphaFade = 0.001f * 1000f / Game1.viewport.Width,
-							alpha = 1f * scale,
-							motion = motion * scale / 4f,
-							acceleration = new Vector2(0f, 0f),
-							interval = 500 * scale,
-							animationLength = 4,
-							totalNumberOfLoops = 999999,
-							layerDepth = local.Y / 10000f,
-							scale = 4f * scale,
-							yPeriodic = true,
-							yPeriodicLoopTime = 2000 * scale,
-							yPeriodicRange = 24 * scale
-						});
-						if (Game1.random.NextDouble() < spriteChance * 10)
-							Game1.playSound("ghost");
-					}
-				} 
+				
 				else if (ModEntry.State.Value.MapNumber == (int)MapNumber.Orange)
 				{
 					if (e.actors.Count < 30 && !e.underwaterSprites.Any() && !e.actors.Any(actor => Vector2.Distance(actor.Tile, who.Tile) < 10))
@@ -396,15 +444,14 @@ namespace Halloween2024
 			}
 		}
 
-		public static void DrawOverlays(SpriteBatch b)
+		public static void DrawTimer(SpriteBatch b,Farmer who, int ms)
 		{
-			Farmer who = Game1.player;
-			int ms = ModEntry.State.Value.EndMs > 0 ? ModEntry.State.Value.EndMs : ModEntry.TotalMs - ModEntry.State.Value.StartMs;
 			string which = ModEntry.Strings.GetValueSafe(ModEntry.State.Value.MapNumber.ToString());
 			string time = Utility.getMinutesSecondsStringFromMilliseconds(ms);
 			string[] texts = [which, time];
 			string s = texts.MaxBy(s => Game1.dialogueFont.MeasureString(s).X);
 
+			Vector2 zero = new(Game1.viewport.X, Game1.viewport.Y);
 			Point timeSize = Game1.dialogueFont.MeasureString(time).ToPoint();
 			Point textSize = Game1.dialogueFont.MeasureString($"{s}\n{s}").ToPoint();
 			Point boxPadding = new(16, 8);
@@ -412,15 +459,16 @@ namespace Halloween2024
 			Point boxPosition = new(x: Game1.viewport.Width / 2 - boxSize.X / 2, y: 0);
 
 			Color color = ModEntry.IsNightmare ? ModEntry.State.Value.MinimapColor : Color.White;
+			float alpha = Math.Min(0.8f, Vector2.Distance(who.Position - zero, boxPosition.ToVector2() + boxSize.ToVector2() / 2) / boxSize.X);
 
 			b.Draw(
 				texture: Game1.fadeToBlackRect,
 				destinationRectangle: new Rectangle(boxPosition, boxSize),
-				color: Color.Black * 0.75f);
+				color: Color.Black * alpha);
 			Game1.drawWithBorder(
 				message: texts[0],
-				borderColor: Color.Black,
-				insideColor: color,
+				borderColor: Color.Black * alpha,
+				insideColor: color * alpha,
 				position: (boxPosition + boxPadding).ToVector2(),
 				rotate: 0f,
 				scale: 1f,
@@ -428,59 +476,47 @@ namespace Halloween2024
 				tiny: false);
 			Game1.drawWithBorder(
 				message: texts[1],
-				borderColor: Color.Black,
-				insideColor: color,
+				borderColor: Color.Black * alpha,
+				insideColor: color * alpha,
 				position: (boxPosition + boxPadding).ToVector2()
 					+ new Vector2(x: textSize.X - timeSize.X, y: textSize.Y) / 2,
 				rotate: 0f,
 				scale: 1f,
 				layerDepth: 1f,
 				tiny: false);
+		}
 
+		public static void DrawMinimap(SpriteBatch b, Farmer who, int ms)
+		{
 			if (ModEntry.State.Value.MinimapTexture is Texture2D minimap && !ModEntry.IsNightmare)
 			{
 				Vector2 v = Vector2.Zero;
 				float baseScale = 2f;
 				float hoverScale = 4f;
-				Vector2 minimapSize = minimap.Bounds.Size.ToVector2() * (ModEntry.State.Value.IsMinimapHovered ? hoverScale : baseScale);
-				float minimapAlpha = Math.Min(0.666f, Vector2.Distance(who.Position, new(Game1.tileSize)) / minimapSize.X);
-				ModEntry.State.Value.IsMinimapHovered = minimapAlpha >= 0.5f && new Rectangle(v.ToPoint(), minimapSize.ToPoint()).Contains(Game1.getMousePosition());
-				float minimapScale = ModEntry.State.Value.IsMinimapHovered
+				Vector2 size = minimap.Bounds.Size.ToVector2() * (ModEntry.State.Value.IsMinimapHovered ? hoverScale : baseScale);
+				float alpha = Math.Min(0.666f, Vector2.Distance(who.Position, new(Game1.tileSize)) / size.X);
+				ModEntry.State.Value.IsMinimapHovered = alpha >= 0.5f && new Rectangle(v.ToPoint(), size.ToPoint()).Contains(Game1.getMousePosition());
+				float scale = ModEntry.State.Value.IsMinimapHovered
 					? hoverScale
 					: baseScale;
 				b.Draw(
 					texture: minimap,
 					position: v,
 					sourceRectangle: null,
-					color: Color.White * (ModEntry.State.Value.IsMinimapHovered ? 1f : minimapAlpha),
+					color: Color.White * (ModEntry.State.Value.IsMinimapHovered ? 1f : alpha),
 					rotation: 0f,
 					origin: Vector2.Zero,
-					scale: minimapScale,
+					scale: scale,
 					effects: SpriteEffects.None,
 					layerDepth: 0f);
 				b.Draw(
 					texture: Game1.fadeToBlackRect,
-					position: v + who.Tile * minimapScale,
+					position: v + who.Tile * scale,
 					sourceRectangle: null,
 					color: Color.Red,
 					rotation: 0f,
 					origin: Vector2.Zero,
-					scale: minimapScale,
-					effects: SpriteEffects.None,
-					layerDepth: 0f);
-			}
-
-			if (who.swimming.Value && ModEntry.State.Value.Texture is Texture2D texture)
-			{
-				int frame = ms / 120 % 10;
-				b.Draw(
-					texture: texture,
-					position: Game1.GlobalToLocal(Game1.viewport, who.Position + new Vector2(0f, who.Sprite.SpriteHeight / 4 * 4)),
-					sourceRectangle: new Rectangle(16 * frame, 0, 16, 16),
-					color: Color.DarkBlue,
-					rotation: 0f,
-					origin: Vector2.Zero,
-					scale: Game1.pixelZoom,
+					scale: scale,
 					effects: SpriteEffects.None,
 					layerDepth: 0f);
 			}
@@ -535,9 +571,14 @@ namespace Halloween2024
 			ModEntry.Strings = ModEntry.LoadStrings();
 		}
 
+		private void GameLoop_Saving(object? sender, StardewModdingAPI.Events.SavingEventArgs e)
+		{
+			ModEntry.RepeatUnfinishedEvents(Game1.player);
+		}
+
 		private void Player_Warped(object? sender, StardewModdingAPI.Events.WarpedEventArgs e)
 		{
-			if (!e.NewLocation.IsTemporary || e.NewLocation.getMapProperty(ModEntry.Strings.GetValueSafe("ID")) is null)
+			if (!ModEntry.EventMap)
 				return;
 
 			ModEntry.SetUpEventLocation(e.NewLocation);
@@ -548,7 +589,28 @@ namespace Halloween2024
 			if (ModEntry.State.Value.StartMs == ModEntry.State.Value.EndMs)
 				return;
 
-			ModEntry.DrawOverlays(e.SpriteBatch);
+			SpriteBatch b = e.SpriteBatch;
+			Farmer who = Game1.player;
+			int ms = ModEntry.State.Value.EndMs > 0 ? ModEntry.State.Value.EndMs : ModEntry.TotalMs - ModEntry.State.Value.StartMs;
+
+			ModEntry.DrawTimer(b, who, ms);
+			ModEntry.DrawMinimap(b, who, ms);
+
+			// Unique map features
+			if (who.swimming.Value && ModEntry.State.Value.Texture is Texture2D texture)
+			{
+				int frame = ms / 120 % 10;
+				b.Draw(
+					texture: texture,
+					position: Game1.GlobalToLocal(Game1.viewport, who.Position + new Vector2(0f, who.Sprite.SpriteHeight / 4 * 4)),
+					sourceRectangle: new Rectangle(16 * frame, 0, 16, 16),
+					color: Color.DarkBlue,
+					rotation: 0f,
+					origin: Vector2.Zero,
+					scale: Game1.pixelZoom,
+					effects: SpriteEffects.None,
+					layerDepth: 0f);
+			}
 		}
 
 		[HarmonyPatch]
@@ -583,12 +645,13 @@ namespace Halloween2024
 					light.position.Value = new(who.Position.X + 24f, who.Position.Y + 48f);
 				}
 
+				// Unique map features
 				if (ModEntry.State.Value.MapNumber == (int)MapNumber.Orange)
 				{
 					ModEntry.UpdatePumpkinMen(__instance, where, who);
 				}
 
-				ModEntry.TryCreateMapSprite(e: __instance);
+				ModEntry.TryCreateSprite(e: __instance);
 			}
 
 			[HarmonyPostfix]
@@ -599,29 +662,35 @@ namespace Halloween2024
 				if (!ModEntry.EventUp || ModEntry.State.Value.IsMinimapHovered)
 					return;
 
-				if (ModEntry.State.Value.MapGoal.X == xTile && ModEntry.State.Value.MapGoal.Y == yTile)
+				Farmer who = Game1.player;
+
+				void end(MapEnding ending)
 				{
 					__instance.EndPlayerControlSequence();
-					__instance.CurrentCommand += ModEntry.State.Value.IsNaughty ? 2 : 1;
+					__instance.CurrentCommand += (int)ending;
 
 					ModEntry.State.Value.MinimapTexture = null;
 					ModEntry.State.Value.EndMs = ModEntry.TotalMs - ModEntry.State.Value.StartMs;
+				}
 
+				if (ModEntry.State.Value.MapGoal.X == xTile && ModEntry.State.Value.MapGoal.Y == yTile)
+				{
+					// Goal
+					end(ModEntry.State.Value.IsNaughty ? MapEnding.Fail : MapEnding.Success);
 					DelayedAction.functionAfterDelay(
-						func: () =>
-						{
-							ModEntry.EndEvent();
-						},
+						func: ModEntry.EndEvent,
 						delay: 7000);
 				}
 				else
 				{
+					// Quit
 					Game1.activeClickableMenu = new ConfirmationDialog(
-						message: ModEntry.Strings.GetValueSafe("Q"),
+						message: ModEntry.Strings.GetValueSafe("QUIT"),
 						onConfirm: (Farmer who) =>
 						{
+							Game1.exitActiveMenu();
+							end(MapEnding.Quit);
 							ModEntry.EndEvent();
-							__instance.skipEvent();
 						});
 				}
 			}
@@ -634,6 +703,7 @@ namespace Halloween2024
 				if (!ModEntry.EventUp)
 					return;
 
+				// Cheat check
 				if (Game1.currentLocation.IsOutOfBounds(position) || !Game1.currentLocation.isTilePassable(who.Tile))
 				{
 					ModEntry.State.Value.IsNaughty = true;
